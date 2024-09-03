436ba12d9b1fb2164667ce3d5ad4acc4fe2b2af6
metromake <143064370+metromake@users.noreply.github.com>
2024-09-03 11:59:39 +0300

    to be tested

--- src/api/controllers/twoFAController.ts
+++ src/api/controllers/twoFAController.ts
@@ -1,36 +1,106 @@
+import {NextFunction, Request, Response} from 'express';
 import CustomError from '../../classes/CustomError';
-// TODO: Import necessary types and models
+import {TokenContent, User, UserWithLevel} from '@sharedTypes/DBTypes';
+import {LoginResponse, UserResponse} from '@sharedTypes/MessageTypes';
+import fetchData from '../../utils/fetchData';
+import OTPAuth from 'otpauth';
+import twoFAModel from '../models/twoFAModel';
+import QRCode from 'qrcode';
+import jwt from 'jsonwebtoken';
 
-// TODO: Define setupTwoFA function
-const setupTwoFA = async (req, res, next) => {
+const setupTwoFA = async (
+  req: Request<{}, {}, User>,
+  res: Response<{qrCodeUrl: string}>,
+  next: NextFunction,
+) => {
   try {
-    // TODO: Register user to AUTH API
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    };
+    const userResponse = await fetchData<UserResponse>(
+      process.env.AUTH_URL + '/api/v1/users',
+      options,
+    );
 
-    // TODO: Generate a new 2FA secret
+    const secret = new OTPAuth.Secret();
 
-    // TODO: Create the TOTP instance
+    const totp = new OTPAuth.TOTP({
+      issuer: 'Derp',
+      label: userResponse.user.email,
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret,
+    });
 
-    // TODO: Store or update the 2FA data in the database
+    await twoFAModel.create({
+      userId: userResponse.user.user_id,
+      email: userResponse.user.email,
+      twoFactorSecret: secret.base32,
+      twoFactorEnabled: true,
+    });
 
-    // TODO: Generate a QR code and send it in the response
+    const imageUrl = await QRCode.toDataURL(totp.toString());
+    res.json({qrCodeUrl: imageUrl});
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
 };
 
-// TODO: Define verifyTwoFA function
-const verifyTwoFA = async (req, res, next) => {
+const verifyTwoFA = async (
+  req: Request<{}, {}, {email: string; code: string}>,
+  res: Response<LoginResponse>,
+  next: NextFunction,
+) => {
   const {email, code} = req.body;
 
   try {
-    // TODO: Retrieve 2FA data from the database
+    const twoFAData = await twoFAModel.findOne({email});
+    if (!twoFAData || !twoFAData.twoFactorEnabled)
+      return next(new CustomError('2FA not enabled', 400));
 
-    // TODO: Validate the 2FA code
+    const totp = new OTPAuth.TOTP({
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret: OTPAuth.Secret.fromBase32(twoFAData.twoFactorSecret),
+    });
+    const isValid = totp.validate({
+      token: code,
+      window: 1,
+    });
 
-    // TODO: If valid, get the user from AUTH API
-
+    if (!isValid) return next(new CustomError('Invalid 2FA code', 400));
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email}),
+    };
+    const userResponse = await fetchData<UserWithLevel>(
+      process.env.AUTH_URL + '/api/v1/users/' + twoFAData.userId,
+      options,
+    );
+    if (!userResponse) return next(new CustomError('User not found', 404));
     // TODO: Create and return a JWT token
-
+    const tokenContent: TokenContent = {
+      user_id: userResponse.user_id,
+      level_name: userResponse.level_name,
+    };
+    if (!process.env.JWT_SECRET) throw new Error('missing JWT_SECRET');
+    const token = jwt.sign(tokenContent, process.env.JWT_SECRET, {
+      expiresIn: '12h',
+    });
+    res.json({
+      token,
+      user: userResponse,
+      message: 'Logged in',
+    });
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
